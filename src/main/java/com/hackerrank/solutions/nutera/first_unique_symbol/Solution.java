eb59736d621ab78bf6e1c5aa25a752da2b092724
Alexandr Shevtsov <ashevtsov@luxoft.com>
2021-02-24 17:44:11 +0300

    first unique symbol refactoring

--- src/main/java/com/hackerrank/solutions/nutera/first_unique_symbol/Solution.java
+++ src/main/java/com/hackerrank/solutions/nutera/first_unique_symbol/Solution.java
@@ -1,39 +1,35 @@
 package com.hackerrank.solutions.nutera.first_unique_symbol;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String[] args) {
-        assertEquals(firstUniqueSymbol("effective"), "c");
-        assertEquals(firstUniqueSymbol("abracadabra"), "c");
-        assertEquals(firstUniqueSymbol("aabb"), "");
+        assertEquals(findFirstUniqueSymbol("effective"), "c");
+        assertEquals(findFirstUniqueSymbol("abracadabra"), "c");
+        assertEquals(findFirstUniqueSymbol("aabb"), "");
+        assertEquals(findFirstUniqueSymbol(null), "");
     }
 
     @SuppressWarnings("SameParameterValue")
     private static void assertEquals(String actual, String expected) {
         Optional.ofNullable(actual)
                 .filter(it -> it.equals(expected))
-                .orElseThrow(() -> new AssertionError(String.format("expected: %s\nactual: %s", expected, actual)));
+                .orElseThrow(() -> new AssertionError(String.format("\nexpected: %s\nactual: %s", expected, actual)));
     }
 
-    public static String firstUniqueSymbol(String s) {
+    public static String findFirstUniqueSymbol(String s) {
         if (s == null) {
             return "";
         }
 
-        Map<Character, Integer> charactersMap = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            Character ch = s.charAt(i);
-            Integer count = charactersMap.get(s.charAt(i));
-            charactersMap.put(ch, count == null ? 1 : count + 1);
-        }
+        Map<Character, Long> charsMap = s.chars()
+                .mapToObj(value -> (char) value)
+                .collect(Collectors.groupingBy(it -> it, Collectors.counting()));
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            Integer count = charactersMap.get(s.charAt(i));
-            if (count == 1) {
+        for (char ch : s.toCharArray()) {
+            if (charsMap.get(ch) == 1) {
                 return Character.toString(ch);
             }
         }
