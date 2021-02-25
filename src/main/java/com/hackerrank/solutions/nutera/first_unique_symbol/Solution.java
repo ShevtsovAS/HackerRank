b6ef94c280b39e0be1a50dee6aa24e871ae43884
Alexandr Shevtsov <ashevtsov@luxoft.com>
2021-02-25 17:49:01 +0300

    findFirstSymbol refactoring

--- src/main/java/com/hackerrank/solutions/nutera/first_unique_symbol/Solution.java
+++ src/main/java/com/hackerrank/solutions/nutera/first_unique_symbol/Solution.java
@@ -1,8 +1,8 @@
 package com.hackerrank.solutions.nutera.first_unique_symbol;
 
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.*;
+
+import static java.util.stream.Collectors.toMap;
 
 public class Solution {
     public static void main(String[] args) {
@@ -19,21 +19,22 @@ public class Solution {
                 .orElseThrow(() -> new AssertionError(String.format("\nexpected: %s\nactual: %s", expected, actual)));
     }
 
-    public static String findFirstUniqueSymbol(String s) {
-        if (s == null) {
-            return "";
-        }
+    public static String findFirstUniqueSymbol(String str) {
+        return Optional.ofNullable(str)
+                .map(Solution::getSymbolsMap)
+                .map(LinkedHashMap::entrySet)
+                .stream().flatMap(Collection::stream)
+                .filter(Solution::isUnique)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse("");
+    }
 
-        Map<Character, Long> charsMap = s.chars()
-                .mapToObj(value -> (char) value)
-                .collect(Collectors.groupingBy(it -> it, Collectors.counting()));
+    private static boolean isUnique(Map.Entry<String, Integer> entry) {
+        return entry.getValue() == 1;
+    }
 
-        for (char ch : s.toCharArray()) {
-            if (charsMap.get(ch) == 1) {
-                return Character.toString(ch);
-            }
-        }
-
-        return "";
+    private static LinkedHashMap<String, Integer> getSymbolsMap(String str) {
+        return Arrays.stream(str.split("")).collect(toMap(it -> it, it -> 1, Integer::sum, LinkedHashMap::new));
     }
 }
