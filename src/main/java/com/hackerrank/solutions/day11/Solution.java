da0a8005c1bbcf9a937763bbf43b54cdcd8a15e9
Alexandr Shevtsov <ashevtsov@luxoft.com>
2021-03-07 04:01:15 +0300

    day11

--- src/main/java/com/hackerrank/solutions/day11/Solution.java
+++ src/main/java/com/hackerrank/solutions/day11/Solution.java
@@ -1,23 +1,16 @@
 package com.hackerrank.solutions.day11;
 
-import java.util.Scanner;
-
 public class Solution {
 
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int[][] arr = new int[6][6];
-        for (int i = 0; i < 6; i++) {
-            String[] arrRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int j = 0; j < 6; j++) {
-                int arrItem = Integer.parseInt(arrRowItems[j]);
-                arr[i][j] = arrItem;
-            }
-        }
-        scanner.close();
+        int[][] arr = new int[][]{
+                {1, 1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0},
+                {1, 1, 1, 0, 0, 0},
+                {0, 0, 2, 4, 4, 0},
+                {0, 0, 0, 2, 0, 0},
+                {0, 0, 1, 2, 4, 0}
+        };
 
         int max = Integer.MIN_VALUE;
         for (int x = 0; x <= 3; x++) {
@@ -36,12 +29,16 @@ public class Solution {
         int sum = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (i % 2 != 0 && j % 2 == 0) {
+                if (isOutOfNeck(i, j)) {
                     continue;
                 }
                 sum += arr[x + i][y + j];
             }
         }
         return sum;
+    }
+
+    private static boolean isOutOfNeck(int i, int j) {
+        return i % 2 != 0 && j % 2 == 0;
     }
 }
