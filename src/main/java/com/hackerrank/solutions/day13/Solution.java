5d838439257e2e0d6f14750a58043bb2176427bd
Aleksandr Shevtsov <ashevtsov@spb.luxoft.com>
2021-12-29 18:41:05 +0300

    day13

---
+++ src/main/java/com/hackerrank/solutions/day13/Solution.java
@@ -0,0 +1,47 @@
+package com.hackerrank.solutions.day13;
+
+import java.util.Scanner;
+
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+class MyBook extends Book {
+    int price;
+
+    public MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    @Override
+    void display() {
+        System.out.printf(
+                "Title: %s%n" +
+                "Author: %s%n" +
+                "Price: %s%n",
+                title, author, price);
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
