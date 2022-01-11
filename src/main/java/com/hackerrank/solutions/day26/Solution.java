5e1c68e7ab643b7436fd6456573133d569ca1691
Aleksandr Shevtsov <ashevtsov@spb.luxoft.com>
2022-01-11 04:49:22 +0300

    day26 - short, but unreadable code

--- src/main/java/com/hackerrank/solutions/day26/Solution.java
+++ src/main/java/com/hackerrank/solutions/day26/Solution.java
@@ -1,52 +1,37 @@
 package com.hackerrank.solutions.day26;
 
+import java.time.LocalDate;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] returnedDate;
-        int[] dueDate;
-        try (Scanner scanner = new Scanner(System.in)) {
-            returnedDate = getDate(scanner);
-            dueDate = getDate(scanner);
-        }
-        System.out.println(getFine(returnedDate, dueDate));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(getFine(getDate(scanner), getDate(scanner)));
     }
 
-    private static int getFine(int[] returnedDate, int[] dueDate) {
-        int yearFine = getFine(returnedDate[2], dueDate[2], overLimit -> 10000);
-        if (yearFine > 0) {
-            return yearFine;
-        } else if (yearFine == -1) {
-            return 0;
-        }
-
-        int monthFine = getFine(returnedDate[1], dueDate[1], overLimit -> 500 * overLimit);
-        if (monthFine > 0) {
-            return monthFine;
-        } else if (monthFine == -1) {
-            return 0;
-        }
-
-        return getFine(returnedDate[0], dueDate[0], overLimit -> 15 * overLimit);
+    private static int getFine(LocalDate returnedDate, LocalDate dueDate) {
+        return returnedDate.isBefore(dueDate) ? 0 : Stream.of(
+                        new SimpleEntry<Function<LocalDate, Integer>, Function<Integer, Integer>>(LocalDate::getYear, overLimit -> 10000),
+                        new SimpleEntry<Function<LocalDate, Integer>, Function<Integer, Integer>>(LocalDate::getMonthValue, overLimit -> 500 * overLimit),
+                        new SimpleEntry<Function<LocalDate, Integer>, Function<Integer, Integer>>(LocalDate::getDayOfMonth, overLimit -> 15 * overLimit)
+                ).map(entry -> getFine(returnedDate, dueDate, entry.getKey(), entry.getValue()))
+                .filter(fine -> fine > 0)
+                .findFirst().orElse(0);
     }
 
-    private static int getFine(int actual, int expected, Function<Integer, Integer> computeFineFunc) {
-        int overLimit = actual - expected;
-        if (overLimit > 0) {
-            return computeFineFunc.apply(overLimit);
-        }
-        if (overLimit < 0) {
-            return -1;
-        }
-        return 0;
+    private static int getFine(LocalDate actual, LocalDate expected, Function<LocalDate, Integer> getPeriodFunc, Function<Integer, Integer> computeFineFunc) {
+        int overLimit = getPeriodFunc.apply(actual) - getPeriodFunc.apply(expected);
+        return overLimit > 0 ? computeFineFunc.apply(overLimit) : 0;
     }
 
-    private static int[] getDate(Scanner scanner) {
-        return Arrays.stream(scanner.nextLine().trim().split(" "))
+    private static LocalDate getDate(Scanner scanner) {
+        int[] date = Arrays.stream(scanner.nextLine().trim().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
+        return LocalDate.of(date[2], date[1], date[0]);
     }
 }
