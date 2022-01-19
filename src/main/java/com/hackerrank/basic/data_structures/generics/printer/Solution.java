86b3eb611dde85e42bd719be987693e460f6ef38
Aleksandr Shevtsov <ashevtsov@spb.luxoft.com>
2022-01-19 15:39:15 +0300

    Data structures - Generics

---
+++ src/main/java/com/hackerrank/basic/data_structures/generics/printer/Solution.java
@@ -0,0 +1,35 @@
+package com.hackerrank.basic.data_structures.generics.printer;
+
+import java.lang.reflect.Method;
+
+//https://www.hackerrank.com/challenges/java-generics/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+class Printer
+{
+    //Write your code here
+    public <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+    }
+}
