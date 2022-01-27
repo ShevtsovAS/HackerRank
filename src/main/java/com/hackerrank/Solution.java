eeff5801f469f28d4754c36b54449d7421815b10
Aleksandr Shevtsov <ashevtsov@spb.luxoft.com>
2022-01-27 19:34:41 +0300

    tmp

--- src/main/java/com/hackerrank/Solution.java
+++ src/main/java/com/hackerrank/Solution.java
@@ -1,22 +1,8 @@
 package com.hackerrank;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static java.util.regex.Pattern.CASE_INSENSITIVE;
-
 public class Solution {
+
     public static void main(String[] args) {
-        String regex = "\\b(\\w+)(\\s+\\1\\b)+";
-        Pattern pattern = Pattern.compile(regex, CASE_INSENSITIVE);
-        String input = "Goodbye bye bye world world world";
-        Matcher matcher = pattern.matcher(input);
-        System.out.println(input);
-        while (matcher.find()) {
-            System.out.println(matcher.group(0));
-            System.out.println(matcher.group(1));
-            input = input.replaceAll(matcher.group(0), matcher.group(1));
-        }
-        System.out.println(input);
+
     }
 }
