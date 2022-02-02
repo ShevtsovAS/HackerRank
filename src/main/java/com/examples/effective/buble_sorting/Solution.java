3eca24a1d18576bb7833f8bc59461427efeeadd8
Aleksandr Shevtsov <ashevtsov@spb.luxoft.com>
2022-02-02 15:45:51 +0300

    Bubble sorting refactoring

--- src/main/java/com/examples/effective/buble_sorting/Solution.java
+++ src/main/java/com/examples/effective/buble_sorting/Solution.java
@@ -13,10 +13,10 @@ public class Solution {
     }
 
     public static void bubbleSort(int[] arr) {
-        for (int i = arr.length - 1; i >= 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    swap(arr, j, j + 1);
+        for (int i = arr.length; i > 0; i--) {
+            for (int j = 1; j < i; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    swap(arr, j - 1, j);
                 }
             }
         }
