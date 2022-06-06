b666f2b07ec6574a1dc10fdff2ee5ad4809b8e04
Aleksandr Shevtsov <mr.alexandr.shevtsov@gmail.com>
2022-06-06 17:15:15 +0300

    Linux shell: slice array (simplest way)

--- src/main/java/com/hackerrank/bash_scripts/slice_array.sh
+++ src/main/java/com/hackerrank/bash_scripts/slice_array.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 #https://www.hackerrank.com/challenges/bash-tutorials-slice-an-array/problem
-declare -a arr
-while read -r line; do
-  arr+=("$line")
-done
-
-echo "${arr[@]}" | cut -f3-7 -d ' '
+mapfile -t arr <<< "$(cat)"
+echo "${arr[@]:3:5}"
