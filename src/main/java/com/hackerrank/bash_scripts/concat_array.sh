1838275bef1e86fe41f9585e22db9c9b8bd260ec
Aleksandr Shevtsov <mr.alexandr.shevtsov@gmail.com>
2022-06-06 18:13:59 +0300

    Linux shell: concatenate array

--- src/main/java/com/hackerrank/bash_scripts/concat_array.sh
+++ src/main/java/com/hackerrank/bash_scripts/concat_array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #https://www.hackerrank.com/challenges/bash-tutorials-concatenate-an-array-with-itself/problem
-mapfile -t arr <<<"$(cat)"
+mapfile -t arr
 result=("${arr[@]}" "${arr[@]}" "${arr[@]}")
 echo "${result[@]}"
