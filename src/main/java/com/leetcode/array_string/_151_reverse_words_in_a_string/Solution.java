29412b05737c81855f4f5f09945056485ad2eab4
Aleksandr Shevtsov <mr.alexandr.shevtsov@gmail.com>
2024-05-23 02:51:09 -0400

    Revert "[leetcode] 151. Reverse Words in a String (most effective solution2)"
    This reverts commit e96dec4ac319826ecc817021bbb6edcd6614e0ce.

--- src/main/java/com/leetcode/array_string/_151_reverse_words_in_a_string/Solution.java
+++ src/main/java/com/leetcode/array_string/_151_reverse_words_in_a_string/Solution.java
@@ -3,46 +3,46 @@ package com.leetcode.array_string._151_reverse_words_in_a_string;
 /**
  * <h1>151. Reverse Words in a String</h1>
  * Given an input string s, reverse the order of the words.<br>
- * <br>
+ *<br>
  * A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.<br>
- * <br>
+ *<br>
  * Return a string of the words in reverse order concatenated by a single space.<br>
- * <br>
+ *<br>
  * Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.<br>
- * <br>
+ *<br>
  * Example 1:<br>
  * Input: s = "the sky is blue"<br>
  * Output: "blue is sky the"<br>
- * <br>
+ *<br>
  * Example 2:<br>
  * Input: s = "  hello world  "<br>
  * Output: "world hello"<br>
  * Explanation: Your reversed string should not contain leading or trailing spaces.<br>
- * <br>
+ *<br>
  * Example 3:<br>
  * Input: s = "a good   example"<br>
  * Output: "example good a"<br>
  * Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.<br>
- * <br>
+ *<br>
  * Constraints:<br>
  * 1 <= s.length <= 104<br>
  * s contains English letters (upper-case and lower-case), digits, and spaces ' '.<br>
  * There is at least one word in s.<br>
- * <br>
+ *<br>
  * Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?<br>
  */
 public class Solution {
     public String reverseWords(String s) {
-        char[] source = s.toCharArray();
         char[] result = new char[s.length()];
         int pos = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
+        for (int i = s.length() - 1; i >= 0 ; i--) {
             if (s.charAt(i) == ' ') continue;
             int j = i - 1;
             while (j >= 0 && s.charAt(j) != ' ') j--;
             if (pos != 0) result[pos++] = ' ';
-            System.arraycopy(source, j + 1, result, pos, i - j);
-            pos += i - j;
+            for (int k = j + 1; k <= i; k++) {
+                result[pos++] = s.charAt(k);
+            }
             i = j;
         }
         return new String(result, 0, pos);
