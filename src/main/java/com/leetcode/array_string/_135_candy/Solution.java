d80b81663a5d2fd0e559da8c03806fa5175c5bca
Aleksandr Shevtsov <mr.alexandr.shevtsov@gmail.com>
2024-05-31 02:46:37 -0400

    [leetcode] 135. Candy (optimization)

--- src/main/java/com/leetcode/array_string/_135_candy/Solution.java
+++ src/main/java/com/leetcode/array_string/_135_candy/Solution.java
@@ -29,30 +29,25 @@ import java.util.Arrays;
 public class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-
-        Arrays.fill(left, 1);
-        Arrays.fill(right, 1);
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
 
         for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) left[i] = left[i - 1] + 1;
+            if (ratings[i] > ratings[i - 1]) candies[i] = candies[i - 1] + 1;
         }
 
+        int result = candies[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) right[i] = right[i + 1] + 1;
+            if (ratings[i] > ratings[i + 1]) candies[i] = Math.max(candies[i], (candies[i + 1] + 1));
+            result += candies[i];
         }
 
-        int candies = 0;
-        for (int i = 0; i < n; i++) {
-            candies += Math.max(left[i], right[i]);
-        }
-
-        return candies;
+        return result;
     }
 
     public static void main(String[] args) {
-        int[] ratings = {1, 3, 2, 2, 1};
+        int[] ratings = {1, 6, 10, 8, 7, 3, 2};
+        // 1 2 5 4 3 2 1
         System.out.println(new Solution().candy(ratings));
     }
 }
